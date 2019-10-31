465b449c0edad563dd7aeb6fa4c396042d41ac91
GHUTI <yannick.remy16@gmail.com>
2019-10-31 16:01:18 +0100

    app2

--- views/books.php
+++ views/books.php
@@ -2,6 +2,20 @@
 <?php ob_start(); ?>
 <h1>LISTE DES LIVRES</h1>
 <p>Bienvenue sur la page des livre</p>
+
+<ul>
+  <?php
+    foreach ($books as $book) { //echo '<li>' .$book['title'].'<li>' ;
+     ?>
+      <li><?php echo $book['title']; ?></li>
+     <?php
+    }
+  ?>
+</ul>
+
+<pre>
+<?php var_dump($books); ?>
+</pre>
 <?php $content = ob_get_clean(); ?>
 
 <?php require('public/index.php'); ?>
