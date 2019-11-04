a5c8d9b5b8f6b9df64b39c62d29885eedf2c3552
GHUTI <yannick.remy16@gmail.com>
2019-11-04 11:02:44 +0100

    blups

--- views/books.php
+++ views/books.php
@@ -1,17 +1,30 @@
 <?php $title = "Liste des livres" ?>
 <?php ob_start(); ?>
-<h1>LISTE DES LIVRES</h1>
-<h3>Bienvenue sur la page des livre</h3>
+<h1>LIST OF BOOKS</h1>
 
-<ul>
+<ul style="list-style:none; margin-left:0; padding-left:0;">
   <div class="row">
     <?php
       foreach ($books as $book) { //echo '<li>' .$book['title'].'<li>' ;
       ?>
 
-      <li>
-        <h1><?php echo $book['title']; ?></h1>
-        <?php echo '<img src="' . $book['imageLink'] .'">'; ?>
+      <li class="li-nop">
+        <div class="card-group">
+          <div class="card blups">
+            <?php echo '<img class="card-img img-nop" src="' . $book['imageLink'] .'">'; ?>
+            <div class="card-body">
+              <h5 class="card-title"><?php echo $book['title']; ?></h5>
+              <p class="card-text">Author : <?php echo $book['author']; ?></p>
+              <p class="card-text">country : <?php echo $book['country']; ?></p>
+              <p class="card-text">language : <?php echo $book['language']; ?></p>
+            </div>
+            <div class="card-footer">
+              <a role="button" class="btn btn-primary btn-block" href="<?php echo $book['link']; ?>">learn more</a>
+            </div>
+          </div>
+        </div>
+
+
 
       </li>
      <?php
